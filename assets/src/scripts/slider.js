6d558c57093fcb79eaa0e9e696ac213b1e19d1a8
Igor M. <samplefines@gmail.com>
2023-11-14 15:48:26 +0300

    optimize slider.js add for touchscreens divSliderDots.ontouchstart = turnOnSliderDots;

--- assets/src/scripts/slider.js
+++ assets/src/scripts/slider.js
@@ -18,21 +18,18 @@ function createSliderDots() {
     }
 
     // добавляем точки для версий сладера с двумя карточками на экране - в дополнение к уже созданным точкам. 
-    //создаём новый счётчик для цикла. (или можно попробовать обнулить i после прекращения выполнения предудущего цикла. Или продолжить счётчик i, отредактировав условие while)
-    let k = 0;
-    while (k < ((sliderCards.length / 2) - (sliderCards.length / 3))) {
+    // продолжаем счётчик i, отредактировав условие while - указав сколько точек должно быть в итоге 
+    while (i < (sliderCards.length / 2)) {
         divSliderDots.insertAdjacentHTML("beforeend", '<div class="slider__dot slider__dot_2card"></div>');
-        k++;
-        console.log(k);
+        i++;
+        console.log(i);
     }
 
     // добавляем точки для версий сладера с одной карточкой на экране
-    let l = 0;
-    // while (l < (sliderCards.length - (sliderCards.length / 3) - ((sliderCards.length / 2) - (sliderCards.length / 3)))) {
-    while (l < (sliderCards.length / 2)) {
+    while (i < sliderCards.length) {
         divSliderDots.insertAdjacentHTML("beforeend", '<div class="slider__dot slider__dot_1card"></div>');
-        l++;
-        console.log(l);
+        i++;
+        console.log(i);
     }
 
 }
@@ -51,7 +48,7 @@ sliderDots[0].classList.add('slider__dot_active');
 sliderCardsCont.style.transition = '0.3s';
 sliderWrapper.style.transition = '0.3s';
 
-let shownImage = 0; //задаём индекс отображаемой картинки (карточки) - (при загрузке страницы отображается первая карточка, её индекс 0 )
+let shownImage = 0; // задаём индекс отображаемой картинки (карточки) - (при загрузке страницы отображается первая карточка, её индекс 0 )
 let slideWidth; // расстояние сдвига карточек при листании (ширина карточки + расстояние между карточками)
 
 function setSlideWidth() {
@@ -75,10 +72,8 @@ function removeSliderDotActive() {
     }
 }
 
-
 btnRight.onclick = () => {
-    // 
-    shownImage++;
+    shownImage++; // увеличиваем индекс отображаемой картинки на 1 (т.к. мы листаем вправо)
     // задаём на какой карточке при листании вперёд нужно будет переходить к первой карточке, в зависимости от ширины экрана (т.е. кол-ва отображаемых карточек в окне слайдера)
     if (window.innerWidth < 825) {
         if (shownImage >= (sliderCards.length)) {
@@ -115,13 +110,12 @@ btnRight.onclick = () => {
             // for 3-card slider (window.innerWidth > 1250)
             sliderDots = divSliderDots.querySelectorAll(".slider__dot:not(.slider__dot_2card):not(.slider__dot_1card)");
             console.log(sliderDots);
-
             sliderDots[Math.round(shownImage / 3)].classList.add('slider__dot_active');
         }
 }
 
 btnLeft.onclick = () => {
-    shownImage--;
+    shownImage--; // уменьшаем индекс отображаемой картинки на 1 (т.к. мы листаем вправо)
 
     // задаём на какую карточку при листании назад с первой краточки нужно будет переходить, в зависимости от ширины экрана (т.е. кол-ва отображаемых карточек в окне слайдера)
     if (window.innerWidth < 825) {
@@ -161,14 +155,13 @@ btnLeft.onclick = () => {
             // for 3-card slider (window.innerWidth > 1250)
             sliderDots = divSliderDots.querySelectorAll(".slider__dot:not(.slider__dot_2card):not(.slider__dot_1card)");
             console.log(sliderDots);
-
             sliderDots[Math.round(shownImage / 3)].classList.add('slider__dot_active');
         }
 }
 
 
 // при наведении (вхождении курсора мыши - т.е. однократно) на контейнер с точками (divSliderDots - .slider__dots) в зависимости от window.innerWidth создаем массив только из отображаемых точек и присваиваем точкам (из вновь созданного массива sliderDots) действие перелистывания слайдера по клику:
-divSliderDots.onmouseenter = () => {
+function turnOnSliderDots() {
 
     removeSliderDotActive();
     // в зависимости от ширины экрана (и кол-ва отображаемых точек) добавляем только отображаемые точки в массив (перезаписывая его):
@@ -189,71 +182,30 @@ divSliderDots.onmouseenter = () => {
             // for 3-card slider (window.innerWidth > 1250)
             sliderDots = divSliderDots.querySelectorAll(".slider__dot:not(.slider__dot_2card):not(.slider__dot_1card)");
             console.log(sliderDots);
-
             sliderDots[Math.round(shownImage / 3)].classList.add('slider__dot_active');
         }
 
     // присваиваем точкам (из вновь созданного массива sliderDots) действие перелистывания слайдера по клику
-
-    if (window.innerWidth < 825) {
-        sliderDots.forEach((sliderDot, i) => {
-            sliderDot.onclick = () => {
+    sliderDots.forEach((sliderDot, i) => {
+        sliderDot.onclick = () => {
+            if (window.innerWidth < 825) {
                 shownImage = i;
-                setSlideWidth();
-                slideToImage(shownImage);
-
-                // убираем со всех точек класс slider__dot_active и вешаем его на ту точку, по которой кликнули
-                removeSliderDotActive();
-                sliderDots[i].classList.add('slider__dot_active');
-            }
-        });
-    }
-    else if (window.innerWidth < 1250) {
-        sliderDots.forEach((sliderDot, i) => {
-            sliderDot.onclick = () => {
+            } else if (window.innerWidth < 1250) {
                 shownImage = i * 2;
-
-                setSlideWidth();
-                slideToImage(shownImage);
-
-                // убираем со всех точек класс slider__dot_active и вешаем его на ту точку, по которой кликнули
-                removeSliderDotActive();
-                sliderDots[i].classList.add('slider__dot_active');
-            }
-        });
-    } else {
-        sliderDots.forEach((sliderDot, i) => {
-            sliderDot.onclick = () => {
+            } else {
                 shownImage = i * 3;
-
-                setSlideWidth();
-                slideToImage(shownImage);
-
-                // убираем со всех точек класс slider__dot_active и вешаем его на ту точку, по которой кликнули
-                removeSliderDotActive();
-                sliderDots[i].classList.add('slider__dot_active');
             }
-        });
-    }
+            setSlideWidth();
+            slideToImage(shownImage);
+
+            // убираем со всех точек класс slider__dot_active и вешаем его на ту точку, по которой кликнули
+            removeSliderDotActive();
+            sliderDots[i].classList.add('slider__dot_active');
+        }
+    });
 }
 
+// turnOnSliderDots();
 
-
-
-// sliderDots.forEach((sliderDot, i) => {
-//     sliderDot.onclick = () => {
-//         shownImage = i * 3;
-
-//         setSlideWidth();
-//         slideToImage(shownImage);
-
-//         // убираем со всех точек класс slider__dot_active и вешаем его на ту точку, по которой кликнули
-//         // вынести в отдельную функцию
-//         for (let sliderDot of sliderDots) {
-//             sliderDot.classList.remove('slider__dot_active');
-//         }
-//         sliderDots[i].classList.add('slider__dot_active');
-//     }
-// })
-
-
+divSliderDots.onmouseenter = turnOnSliderDots;
+divSliderDots.ontouchstart = turnOnSliderDots;
