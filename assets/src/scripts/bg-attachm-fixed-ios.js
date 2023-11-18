c9820166797851c1f43937bcda3ee0f7228620b3
Igor M. <samplefines@gmail.com>
2023-11-18 23:02:36 +0300

    improved previous commit

--- assets/src/scripts/bg-attachm-fixed-ios.js
+++ assets/src/scripts/bg-attachm-fixed-ios.js
@@ -4,18 +4,26 @@ let offset;
 
 window.onscroll = (e) => {
     scrolledY = scrollY;
-    orderingOffset = ordering.offsetTop;
+    orderingOffset = ordering.offsetTop; // .offsetTop - расстояние в пикселях от элемента до верхней границы его родителя (т.е. расстояние от .ordering до начала body (т.е. страницы) )
     console.log(scrolledY);
     // console.log(scrollY);
     console.log(orderingOffset);
-    // ordering.style.backgroundPosition = 'center ' + (scrolledY - 1850) + 'px';
-    ordering.style.backgroundPosition = 'center ' + (scrolledY - orderingOffset - 314) + 'px';
-    console.log(scrolledY - 1850);
-    console.log(scrolledY - orderingOffset);
 
-    // ordering.style.backgroundPosition = 'center ' + (100) + 'px';
+    // чисто для примерки
+    // ordering.style.backgroundPosition = 'center ' + (scrolledY - 1850) + 'px';
+    // console.log(scrolledY - 1850);
     // 1850 - вручную подобранный коэфф. смещения - по факту это расстояние от начала (верха) страницы до .ordering
-    // 314 - коэфф для красивого отображения (т.е. тоже что и 15% в CSS для десктопа)
+
+    if (window.innerWidth >= 768) {
+        ordering.style.backgroundPosition = 'center ' + (scrolledY - orderingOffset - Math.max((312 / window.innerWidth), 312)) + 'px';
+        // console.log(scrolledY - orderingOffset);
+        // 312 - коэфф для красивого отображения (подобран, т.к. JS даёт нам scrolledYи orderingOffset в пикселях. 1200 - ширина wrapper-a). т.е. тоже что и 26% в CSS для десктопа 312/1200 = 26%
+
+    } else {
+        ordering.style.backgroundPosition = 'center ' + (scrolledY - orderingOffset - (612 / window.innerWidth)) + 'px';
+    }
+    // ordering.style.backgroundPosition = `center calc(${(scrolledY - orderingOffset)}px - 26%)`; - в инлайновых стилях (которые создаёт JS) не работает CSS функция calc() - рассчитываем (выше) через JS )
+
 }
 
-ordering.style.backgroundPosition = 'center ' + (scrolledY - orderingOffset - 314) + 'px';
+// ordering.style.backgroundPosition = 'center ' + (scrolledY - orderingOffset - 314) + 'px';
