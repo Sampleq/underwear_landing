c7b71cabe7a95378616749591f56750ff272486f
Igor M. <samplefines@gmail.com>
2023-11-22 23:22:03 +0300

    adapt expanding animation of .ordering section  up to 1920p - OK

--- assets/src/scripts/fullscreen-ordering.js
+++ assets/src/scripts/fullscreen-ordering.js
@@ -3,7 +3,7 @@ const orderingImgCont = document.querySelector('.ordering__img-cont');
 const orderingImgContOuter = document.querySelector('.ordering__img-cont-outer');
 const ordering = document.querySelector('.ordering');
 const orderingOuter = document.querySelector('.ordering-outer');
-// const footer = document.querySelector('.footer');
+const footer = document.querySelector('.footer');
 
 // ! selecting an <html> tag element - 3 ways:
 // const html = document.getElementsByTagName('html')[0];
@@ -18,15 +18,23 @@ function enterFullscreenSection() {
         html.style.scrollPaddingTop = 0;
         // задаем плавность перехода блока с фоном (ordering)
         ordering.style.transition = '0.5s';
+
+        // из CSS ((100vh - 55vh) / 2) = 22.5vh . 55vh - это высота ordering (и orderingOuter)
+        footer.style.height = '22.5vh';
+
         // скроллим блок с фоном (ordering) в центр экрана
         ordering.scrollIntoView({ block: "center" });
 
         //задаём отсрочку в 0.5 секунды чтобы страница успела проскроллиться до блока с фоном (ordering) и/или отыгралась анимация увеличения картинки, после этого делаем анимацию разворачивания
         setTimeout(() => {
-            // убираем плавность прокрутки (скроллинга) страницы, разворачиваем блок-обёртку на всю высоту экрана и скроллим до верхней границы блока-обёртки - все эти (три) действия происходят мгновенно - пользователь видит на экране только появление контента соседних блоков
+            // убираем плавность прокрутки (скроллинга) страницы, разворачиваем блок-обёртку на всю высоту экрана и cкроллим блок обертку в центр экрана (т.е. он займёт весь экран) - все эти (три) действия происходят мгновенно - пользователь видит на экране только появление контента соседних блоков
             html.style.scrollBehavior = 'auto';
             orderingOuter.style.height = '100vh';
-            window.scrollBy(0, ((window.innerHeight - ordering.offsetHeight) / 2));
+            //  убрал - т.к. это работало без глюка только до 1280х800
+            // window.scrollBy(0, ((window.innerHeight - ordering.offsetHeight) / 2));
+            // !!! нужно временно добавить высоты футеру ( и с задержкой убрать в этой же функции) чтобы не было "скачка" на больших разрешениях, где нельзя промотать ordering.scrollIntoView({ block: "center" }); ( добавлять до line 22) в центр экрана !!!
+            orderingOuter.scrollIntoView({ block: "center" });
+
 
             // разворачиваем блок с фоном (ordering) на всю высоту экрана (происходит с анимацией благодаря раннее присвоинному transition)
             ordering.style.height = '100vh';
@@ -42,6 +50,9 @@ function enterFullscreenSection() {
 
             // возвращаем плавность скролла - на случай если пользователь будет пользоваться сайтом после использования "переключения" багофичей)
             html.style.scrollBehavior = 'smooth';
+
+            // возвращаем футер к нормальному размеру, пересчитывая высоту по макету  135px в vh. 10 - это размер шрифта в HTML (переделать в переменную и брать из CSS!!!)
+            footer.style.height = `${100 * (13.5 * 10) / window.innerHeight}vh`;
         }, 500);
     } else {
         // если высота экрана больше высоты блока с фоном (ordering) - только центрируем и вешаем однократный листнер - на случай увеличения высоты экрана (окна браузера) или повторного наведени после скролла
@@ -54,8 +65,9 @@ function enterFullscreenSection() {
 orderingImgCont.addEventListener('mouseenter', enterFullscreenSection, { once: true });
 
 function exitFullscreenSection() {
-    // использовал для возмеожности прокрутки до верхней границы блока с фоном - после мгновн=енного разворачивания блока-обёртки блока с фоном - увеличение футера - не требуется
-    // footer.style.height = '100vh';
+    //  для возможности прокрутки блока с фоном в центр экрана (на больших экранах) - после мгновенного сворачивания блока-обёртки блока с фоном - мгоувеличение футера - 
+    footer.style.transition = '0.5s';
+    footer.style.height = '22.5vh';
 
     // возвращаем плавность скролла (на случай, если пользователь скроллил страницу с развернуым блоком с фоном)
     html.style.scrollBehavior = 'smooth';
@@ -85,8 +97,9 @@ function exitFullscreenSection() {
             orderingOuter.style.removeProperty('height');
             ordering.scrollIntoView({ block: "center" });
 
-            // - увеличение футера - не требуется
-            // footer.style.removeProperty('height');
+            // footer.style.height = '13.5rem';
+            // возвращаем футер к нормальному размеру, пересчитывая высоту по макету  135px в vh. 10 - это размер шрифта в HTML (переделать в переменную и брать из CSS!!!)
+            footer.style.height = `${100 * (13.5 * 10) / window.innerHeight}vh`;
 
             // возвращаем плавность скролла - для дальнейшего просмотра сайта
             html.style.scrollBehavior = 'smooth';
