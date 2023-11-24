9b3b6d1112f8f5792b1fa4824eacc4615b101319
Igor M. <samplefines@gmail.com>
2023-11-24 23:44:41 +0300

    added button_up

---
+++ assets/src/scripts/button_up.js
@@ -0,0 +1,34 @@
+const btnUp = document.getElementById('btn_up');
+if (window.scrollY > (1.1 * window.innerHeight)) {
+    btnUp.style.visibility = 'visible';
+    btnUp.style.opacity = 1;
+}
+
+document.onscroll = (event) => {
+    // console.log(event);
+    // console.log(window.scrollY);
+
+    // if (window.scrollY > 1000) {
+    if (window.scrollY < (1.1 * window.innerHeight)) {
+        // btnUp.style.display = 'none';
+        btnUp.style.visibility = 'hidden';
+        btnUp.style.opacity = 0;
+    } else {
+        console.log(window.innerHeight);
+        // btnUp.style.display = 'block';
+        btnUp.style.visibility = 'visible';
+        btnUp.style.opacity = 1;
+    }
+
+}
+
+btnUp.onclick = () => {
+    // console.log('btnUp');
+    // window.scrollTo(0, 0) - просто координаты без указания типа перемотки(моментально или плавно)
+    window.scrollTo(
+        {
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+};
